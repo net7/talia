2f898c033668c1aeb5bd5ce95f4789646c25176c
daniel <ghub@limitedcreativity.org>
2007-06-18 16:33:15 +0000

    Changed model and documentation
    git-svn-id: http://svn.talia.discovery-project.eu/talia/Talia/trunk@21 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- doc/samples/core_sample.rb
+++ doc/samples/core_sample.rb
@@ -1,67 +1,80 @@
 =begin
   This is a sample file that explores the possible mechanisms to be used in the
   Source class by giving concrete examples of sources.
+  
+  This "native" syntax has been drafted to map well to RDFS. Since the Talia partners
+  will only suppply RDFS descriptions, more advanced elements are left
+  out intentionally.
+  
+  This describes the ontology/type classes and relation types in the
+  system. The objects representing the ontology will be built 
+  dynamically at runtime.
+  Source class by giving concrete examples of sources.
 =end
 
-class RelatesAsPerson < TaliaCore::RelatesAs
-  # The :any keyword will allow any type. This means
-  # that it affects the global superclass instead of a
-  # specific type.
-  relates_to :any, :observer
+# The source types map to RDFS class or similiar elements
+source_type :Person 
+
+source_type :Author do
+  # defines the URI for the type. If nothing is given, the
+  # URI defaults to <local_domain>/sourcetype/<name>
+  uri "http://something/author"
 end
 
+source_type :Essay do
+  data_elements :text, :pdf, :structured
+end
 
-class Essay < TaliaCore::Source
-  # This identifies the "Relationship classes" for this
-  # source type.
-  # The source class inherits the relations defined there.
-  relates_as :work
-  relates_as :citeable
+source_type Path do
+  # This maps for example to the "ordered" container type in RDFS
+  # However, this will also enable the respective behaviour in 
+  # Sources which use this type
+  # In general, the subtype_of will work like RDFS subclasses
+  # TODO: How will this work?
+  subtype_of :Ordered
+end
+
+source_type :Work
+
+# The relation types map to RDFS property or similiar types
+relation_type :Author do
+  # This corresponds to the "range" in RDFS and indicates
+  # that this property has the given Source types as objects
+  # Note that RDFS semantics are a bit tricky, see also
+  # http://jena.sourceforge.net/how-to/rdf-frames.html
+  # TODO: Figure out how this is going to work...
+  range :Essay, :Work, :Path
+  # equivalents? e.g. object :Person ???
   
-  has_data_types :text, :pdf, :structured
-end
-
-class Path < TaliaCore::Source  
-  relates_as :work
-  relates_as :citeable
+  # This is the some for the RDFS "domain" statement,
+  # which identifies the object classes for the subject(s)
+  domain :Person
+  # euivalents? e.g. subjects ... ?
   
-  # This class mixes direct relations and 
-  # relates_as
-  relates_to :part, :path
-  relates_to :page, :path
+  # This has no direct RDFS equivalent. It identifies the
+  # name to be used for the "inverse" relation. 
+  # E.g. the relation is <work> <author> <person>
+  # then the inverse relation could be called 
+  # <person> <publication> <work>
+  # If this is not present, the name will be built
+  # as <predicate>_of (e.g. "author_of")
+  # TODO: Is this really needed, or is the automatic thing enough.
+  # TODO: Check how it's handled in ActiveRDF
+  inverse_name :Publication
 end
 
-
-class RelatesAsWork < TaliaCore::RelatesAs
-  # Multiciplity expressed through pluralization?
-  relates_to :person, :author, :mandatory
-  relates_to :metatdata, :metadata, :mandatory
-  relates_to :citeable, :citation
-end
-
-class RelatesAsCiteable < TaliaCore::RelatesAs
-  # This statement is optional. If it was missing,
-  # the "backlink" would have been inserted
-  # automatically.
-  relates_from :work, :citation
-end
-
-
-
-
-# Relation class: Suffix "Relation" to distinguish from 
-# "Author" source class (just an example). 
-# For the functions used in the examples above, this
-# suffix will automatically be added
-class AuthorRelation < TaliaCore::Relation
-  # This defines the accessor name and cardinality for the
-  # start and end points
-  startpoint :multiple, :accessor => "authors"
-  endpoint :multiple, :accessor => "works"
+relation_type :Reference do
+  range :Work
   
-  add_to_startpoint :primary_author do
-    find(XXX)
-  end
+  # If any of range/domain is missing, the relation is always
+  # "global", which means that it isn't specific to any source
+  # classes.
+end
+
+relation_type :Citation do
+  # There are also relation subtypes.
+  # TODO: How should this work, is this urgently needed?
+  subtype_of :Reference
 end
 
 class PathRelation < TaliaCore::Relation
