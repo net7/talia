24eabc4d4d8b615b6fd0498ed1c07cb2c8c61bd2
daniel <ghub@limitedcreativity.org>
2007-07-26 18:29:29 +0000

    added tesly reporting for the core
    git-svn-id: http://svn.talia.discovery-project.eu/talia/Talia/trunk@56 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- vendor/plugins/talia_core/test/test_helper.rb
+++ vendor/plugins/talia_core/test/test_helper.rb
@@ -2,6 +2,13 @@ require File.dirname(__FILE__) + "/../lib/talia_core"
 
 require 'active_record/fixtures'
 
+# Check for the tesly adapter, and load it if it's there
+if(File.exists?(File.dirname(__FILE__) + '/tesly_reporter.rb'))
+  printf("Continuing with tesly \n")
+  require File.dirname(__FILE__) + '/tesly_reporter'
+end
+
+
 module TaliaCore
   
   class TestHelper
