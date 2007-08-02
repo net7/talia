b8ce2fa8b35883ef8e419d03370541a1de7250bf
daniel <ghub@limitedcreativity.org>
2007-08-02 18:54:13 +0000

    Incorporated some useful suggestions from ticket #197
    git-svn-id: http://svn.talia.discovery-project.eu/talia/Talia/trunk/vendor/plugins/talia_core@83 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- vendor/plugins/talia_core/lib/talia_core/rdf_resource_wrapper.rb
+++ vendor/plugins/talia_core/lib/talia_core/rdf_resource_wrapper.rb
@@ -1,29 +1,23 @@
+require 'active_rdf'
+require 'semantic_naming'
+
 module TaliaCore
-  
-  require 'active_rdf'
-  require 'semantic_naming'
   
   # This extends the Resource from ActiveRDF with some functionality
   # that is needed for Talia operation
   class RdfResourceWrapper < RDFS::Resource
     
-    # Wrap the accessor to get Source objects instead of
-    # RDFS::Resource
+    # Wrap the accessor to get Source objects instead of RDFS::Resource
     def [](uri)
       predicate = super(uri)
-      if(predicate.is_a?(RDFS::Resource))
-        predicate = Source.new(predicate.uri)
-      end
+      predicate = Source.new(predicate.uri) if(predicate.is_a?(RDFS::Resource))
       
-      return predicate
+      predicate
     end
     
-    # Wrap the accessor to set Source objects instead of
-    # RDFS::Resourcce
+    # Wrap the accessor to set Source objects instead of RDFS::Resourcce
     def []=(uri, value)
-      if(value.is_a?(Source))
-        value = RDFS::Resource.new(value.uri.to_s)
-      end
+      value = RDFS::Resource.new(value.uri.to_s) if(value.is_a?(Source))
       super(uri, value)
     end
     
@@ -39,16 +33,9 @@ module TaliaCore
     
     protected
     
-    # Converts an array of resources into an array of 
-    # Predicates
+    # Converts an array of resources into an array of Predicates
     def convert_resource_array(resources)
-      uris = Array.new
-      
-      for resource in resources do
-        uris.push(N::Predicate.new(resource.uri.to_s))
-      end
-      
-      uris
+      resources.collect { |r| N::Predicate.new(r.uri.to_s) } 
     end
     
   end
