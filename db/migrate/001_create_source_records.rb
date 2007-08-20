473f7ca7dc27decdafe51a738c4976727e21873b
daniel <ghub@limitedcreativity.org>
2007-08-20 12:35:08 +0000

    Fixed migrations temporarily
    git-svn-id: http://svn.talia.discovery-project.eu/talia/Talia/trunk@85 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- db/migrate/001_create_source_records.rb
+++ db/migrate/001_create_source_records.rb
@@ -2,6 +2,7 @@ class CreateSourceRecords < ActiveRecord::Migration
   def self.up
     create_table "source_records", :force => true do |t|
       t.column :uri,            :string, :null => false
+      t.column :name,           :string
       t.column :workflow_state, :integer, :null => false
       t.column :primary_source, :bool, :null => false
     end
