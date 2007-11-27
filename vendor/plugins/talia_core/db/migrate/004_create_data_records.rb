1cd3d2d5078c876c983dfb619fe431a054eb5c44
nucci <mik.nucci@gmail.com>
2007-11-27 09:33:44 +0000

    1. Some little modifications on redland adapter: inserted the reconnect paramenter for MySQL storage
    git-svn-id: http://svn.talia.discovery-project.eu/talia/talia_core/trunk@222 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- vendor/plugins/talia_core/db/migrate/004_create_data_records.rb
+++ vendor/plugins/talia_core/db/migrate/004_create_data_records.rb
@@ -6,10 +6,18 @@ class CreateDataRecords < ActiveRecord::Migration
       t.column :location,           :string,  :null => false
     end
    
+    # Create the index
     add_index :data_records, :source_record_id, :unique => false
+    
+    # Create the foreign key
+    execute "alter table data_records add constraint data_records_fkey foreign key (source_record_id) references source_records(id)"
   end
 
   def self.down
+    # drop the foreign key
+    execute "alter table data_records drop foreign key data_records_fkey"
+    
+    # drop the table
     drop_table "data_records"
   end
 
