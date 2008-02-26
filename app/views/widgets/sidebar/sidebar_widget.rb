542a7d6a2170853d20c8cd0a1509349311c45e8d
daniel <ghub@limitedcreativity.org>
2008-02-26 17:04:36 +0000

    Changed sidebar configuration/handling to fix #388 - ordering of sidebar tabs will now always be the same as in the config file.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_app@449 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- app/views/widgets/sidebar/sidebar_widget.rb
+++ app/views/widgets/sidebar/sidebar_widget.rb
@@ -6,31 +6,31 @@ class SidebarWidget < Widgeon::Widget
   def before_render
     @top_tabs = []
     @bottom_tabs = []
-    new_tabs = {}
+    new_tabs = []
     
     # Create bottom and top lists
-    tabs.each do |name, settings|
+    tabs.each do |tab|
       # Add a setting so the tab can check if it's the active one
-      settings['is_active'] = (@active_tab == name)
-      tab_obj = SidebarTab.new(settings, @controller)
-      if(settings['position'] == "bottom")
+      tab['is_active'] = (@active_tab == tab['id'])
+      tab_obj = SidebarTab.new(tab, @controller)
+      if(tab['position'] == "bottom")
         @bottom_tabs << tab_obj
       else
         @top_tabs << tab_obj
       end
-      new_tabs[name] = tab_obj
+      new_tabs << tab_obj
     end
     
     tabs = new_tabs
     
     @active_tab_options = {} unless(@active_tab_options)
-    @tab_widget = tabs[@active_tab]['content']
+    @tab_widget = the_active_tab['content']
   end
   
   
   # Gets the currently active tab
   def the_active_tab
-    tabs[@active_tab]
+    @the_active_tab ||= tabs.find { |tab| tab['id'] == @active_tab }
   end
   
   # Gets the title for the sidebar. If the 'sidebar_title' property is set,
