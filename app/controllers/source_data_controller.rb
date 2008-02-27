db285577afa8bf38b05fca020aed180ac9e8fb5e
gluca <guidi.luca@gmail.com>
2008-02-27 11:25:03 +0000

    Removing unnecessary actions from SourceDataController. Little refactor of the show action.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_app@452 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- app/controllers/source_data_controller.rb
+++ app/controllers/source_data_controller.rb
@@ -1,41 +1,9 @@
 class SourceDataController < ApplicationController
-
-  # GET /sources
-  # GET /sources.xml
-  def index
-  end
-
-  # GET /sources/1
-  # GET /sources/1.xml
+  # GET /source_data/1
   def show
-    @source_data = TaliaCore::DataRecord.find(:first, :conditions => ["type = ? AND location = ?", params[:type].camelize, params[:location]] )
-    raise ActiveRecord::RecordNotFound if @source_data.nil?
+    @source_data = TaliaCore::DataRecord.find_by_type_and_location!(params[:type], params[:location])
     send_data @source_data.content_string, :type => @source_data.mime_type,
                                     :disposition => 'inline',
                                     :filename => params[:location]
-  end
-
-  # GET /sources/new
-  # GET /sources/new.xml
-  def new
-  end
-
-  # GET /sources/1/edit
-  def edit
-  end
-
-  # POST /sources
-  # POST /sources.xml
-  def create
-  end
-
-  # PUT /sources/1
-  # PUT /sources/1.xml
-  def update
-  end
-
-  # DELETE /sources/1
-  # DELETE /sources/1.xml
-  def destroy
   end
 end
