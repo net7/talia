ee591cae281cb1a463b552c33a5f5ceb07fd81cc
gluca <guidi.luca@gmail.com>
2008-04-08 10:46:27 +0000

    Basic RDF admin pane.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_app@548 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- app/helpers/admin/sources_helper.rb
+++ app/helpers/admin/sources_helper.rb
@@ -1,2 +1,8 @@
 module Admin::SourcesHelper
+  def add_another_predicate(namespace)
+    link_to_function "add another" do |page|
+      page.insert_html :bottom, namespace.underscore, :partial => 'predicate', :object => TaliaCore::Source.new(''), :locals => {:namespace => namespace, :name => ''}
+      page[namespace.underscore].select('.predicate').last.visual_effect :highlight
+    end
+  end
 end
