802bde5766e8f18f9e4c44e42c9f7fcb46aab500
gluca <guidi.luca@gmail.com>
2008-04-09 14:06:03 +0000

    Improving functional tests for RDF admin pane.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_app@558 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- test/functional/admin/sources_controller_test.rb
+++ test/functional/admin/sources_controller_test.rb
@@ -1,15 +1,6 @@
 require File.dirname(__FILE__) + '/../../test_helper'
 
 class Admin::SourcesControllerTest < ActionController::TestCase
-  def setup
-    @controller = Admin::SourcesController.new
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new
-    
-    # TODO why @id it isn't loaded?
-    @id = "http://www.talia.discovery-project.org/sources/something"
-  end
-  
   def test_should_get_index
     login_as :admin
     get :index
@@ -19,13 +10,63 @@ class Admin::SourcesControllerTest < ActionController::TestCase
 
   def test_should_get_edit
     login_as :admin
-    get :edit, :id => "something"
+    get :edit, :id => source.label
     assert_response :success
+    
+    assert_select "#source_uri[value=?]", N::LOCAL + source.label
   end
 
   def test_should_update_source
     login_as :admin
-    put :update, :id => "something", :source => { }
+    put :update, :id => source.label, :source => { }
     assert_redirected_to :action => 'index'
+  end
+    
+  def test_should_add_relation_with_existing_source
+    login_as :admin
+    put :update, :id => source.label, :source => params
+    assert(source.direct_predicates_sources.include?("#{N::LOCAL}one"))
+  end
+  
+  def test_should_add_source_relation_with_unexistent_source
+    login_as :admin
+    put :update, :id => source.label, :source => params(predicates_attributes_for_unexistent_source)
+    assert(TaliaCore::Source.exists?(N::LOCAL + 'four'))
+    assert(source.direct_predicates_sources.include?("#{N::LOCAL}Four"))
+  end
+  
+  def test_should_remove_source_relation
+    login_as :admin
+    source.talias::attribute << TaliaCore::Source.find('two')
+    put :update, :id => source.label, :source => params(predicates_attributes_for_destroyable_relation)
+    assert(!source.direct_predicates_sources.include?("#{N::LOCAL}two"))
+  end
+  
+  private
+  def source
+    @source ||= TaliaCore::Source.find('something')
+  end
+  
+  def params(attributes = predicates_attributes)
+    {"uri"=>N::LOCAL + source.label, "primary_source"=>"false" }.merge(attributes)
+  end
+  
+  def predicates_attributes
+    {"predicates_attributes"=>[{"name"=>"attribute", "uri"=>N::LOCAL.to_s, "namespace"=>namespace, "titleized"=>'One'}]}
+  end
+  
+  def predicates_attributes_for_unexistent_source
+    {"predicates_attributes"=>[{"name"=>"attribute", "uri"=>N::LOCAL.to_s, "namespace"=>namespace, "titleized"=>'Four'}]}
+  end
+  
+  def predicates_attributes_for_destroyable_relation
+    {"predicates_attributes"=>[{"name"=>"attribute", "uri"=>N::LOCAL.to_s, "namespace"=>namespace, "titleized"=>'Two', "should_destroy" => '1'}]}
+  end
+  
+  def namespace
+    unless N::URI.shortcut_exists?(:talias)
+      N::Namespace.shortcut(:talias, "http://trac.talia.discovery-project.eu/wiki/StructuralOntology#") 
+    end
+    :talias
   end
 end
