8a303e6331ff7422be1f28262584ecd65aec3ec5
gluca <guidi.luca@gmail.com>
2008-04-16 11:16:36 +0000

    RDF admin pane: adding 'add' and 'remove' images.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_app@593 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- app/helpers/admin/sources_helper.rb
+++ app/helpers/admin/sources_helper.rb
@@ -1,6 +1,6 @@
 module Admin::SourcesHelper
   def add_another_predicate(namespace)
-    link_to_function "add another" do |page|
+    link_to_function image_tag('add.png', :size => '12x12', :alt => 'Add another predicate for '+namespace, :class => 'add') do |page|
       page.insert_html :bottom, namespace.underscore, :partial => 'predicate', :object => TaliaCore::Source.new(''), :locals => {:namespace => namespace, :name => ''}
       page[namespace.underscore].select('.predicate').last.visual_effect :highlight
     end
