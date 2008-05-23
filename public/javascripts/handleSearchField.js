4d200112919549bb1ac390b87882d8dc430d3874
gluca <guidi.luca@gmail.com>
2008-05-23 13:20:20 +0000

    Rewritten handleSeachField in order to improve OOP and cross browser scripting.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_app@657 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- public/javascripts/handleSearchField.js
+++ public/javascripts/handleSearchField.js
@@ -1,55 +1,37 @@
-function addEvent(obj, evType, fn, useCapture){
-  if (obj.addEventListener){
-    obj.addEventListener(evType, fn, useCapture);
-    return true;
-  } else if (obj.attachEvent){
-    var r = obj.attachEvent("on"+evType, fn);
-    return r;
-  } else {
-    alert("Handler could not be attached");
-  }
-}
+var InputSearch = Class.create({
+	initialize: function(id, label){
+		this.element = $(id);
+		this._form = this.element.up('form');
+		this.label = label || 'Cerca';
+		this.element.value = this.label;
+		this.registerHandlers();
+	},
+	registerHandlers: function(){
+		if(Prototype.Browser.Safari){
+	    this._form.addClassName('issafari');
+			this.element.writeAttribute({
+				type: 'search',
+				autosave: 'at.ajaxian.search',
+				results: '5',
+				placeholder: this.label
+			});
+		} else {
+			this.element.observe('focus', this.handleFocus.bindAsEventListener(this));
+			this.element.observe('blur', this.handleBlur.bindAsEventListener(this));
+		}
+	},
+	handleFocus: function(){
+		this.element.value = '';
+		this._form.setStyle('background: transparent url("/images/search_field/bg_search_focus.gif") no-repeat 10px 7px;');
+		this.element.addClassName('focus');
+	},
+	handleBlur: function(){
+		this.element.value = this.label;
+		this._form.setStyle('background: transparent url("/images/search_field/bg_search.gif") no-repeat 10px 7px;');
+		this.element.removeClassName('focus');
+	}
+});
 
-var DEF_VAL   = "Cerca"; // Default Value
-var isSafari  = ((parseInt(navigator.productSub)>=20020000)&&     // detecting WebCore
-               (navigator.vendor.indexOf("Apple Computer")!=-1));
-
-function replaceSearchField() {
-  // Replaces normal input text field with safari's neat search field
-  if (!document.getElementById) return;
-  var searchForm  = document.getElementById('searchform');
-  var searchField = document.getElementById('s');
-  if (!searchField) return;
-  if (isSafari) {
-    searchForm.className = 'issafari';
-    searchField.setAttribute('type', 'search');
-    searchField.setAttribute('autosave', 'at.ajaxian.search');
-    searchField.setAttribute('results', '5');
-    searchField.setAttribute('placeholder', DEF_VAL);
-  } else {
-    addEvent(searchField, 'focus', focusSearch, false);
-    addEvent(searchField, 'blur',  blurSearch, false);
-    if (searchField.value=='') searchField.value = DEF_VAL;
-  }
-  
-}
-
-function focusSearch() {
-  // removes the initial value from the search field
-  if (this.value==DEF_VAL) {
-    this.value = '';
-    $('searchform').setStyle('background: transparent url("/images/search_field/bg_search_focus.gif") no-repeat 10px 7px;');    
-    this.setAttribute('class', 'focus');
-  }
-}
-
-function blurSearch() {
-  // assigns default value, if no value has been entered
-  if (this.value=='') {
-    this.value = DEF_VAL;
-    $('searchform').setStyle('background: transparent url("/images/search_field/bg_search.gif") no-repeat 10px 7px;');
-    this.removeAttribute('class', 'focus');
-  }
-}
-
-addEvent(window, 'load', replaceSearchField, false);
+document.observe('dom:loaded', function(){
+  new InputSearch('s');	
+});
