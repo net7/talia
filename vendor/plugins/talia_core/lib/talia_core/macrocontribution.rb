c7fc0ce5eae59534c0ad121471e8375654c99b3f
danilo <danilo@2960d7dc-a92c-0410-a2fe-c68badc242c0>
2008-07-01 09:40:25 +0000

    added some widgets and views for the simple mode (Facsimile Edition) modified the routes.rb file to point at the home page
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_core@708 2960d7dc-a92c-0410-a2fe-c68badc242c0

---
+++ vendor/plugins/talia_core/lib/talia_core/macrocontribution.rb
@@ -0,0 +1,28 @@
+# require 'objectproperties' # Includes the class methods for the object_properties
+require 'local_store/source_record'
+require 'local_store/data_record'
+require 'local_store/macrocontribution_record'
+require 'macrocontribution_entry'
+require 'local_store/macrocontribution_entry_record'
+require 'local_store/workflow/workflow_record'
+require 'pagination/source_pagination'
+require 'query/source_query'
+require 'active_rdf'
+require 'semantic_naming'
+require 'dummy_handler'
+require 'rdf_resource'
+require 'type_list'
+
+module TaliaCore
+  
+  # This represents a macrocontribution.
+  #
+  # Everything valid for a normal source is also valid here
+  #
+  # It offers some methods for dealing with Macrocontributions  
+  class Macrocontribution < Source
+  end  
+    
+      
+end
+  
