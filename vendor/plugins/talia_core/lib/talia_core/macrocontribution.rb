789e24c48bbde8c15d8b50f3ac583a59ae02b041
danilo <danilo@2960d7dc-a92c-0410-a2fe-c68badc242c0>
2008-07-05 14:26:51 +0000

    deleted a broking line in macrocontribution.rb
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_core@720 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- vendor/plugins/talia_core/lib/talia_core/macrocontribution.rb
+++ vendor/plugins/talia_core/lib/talia_core/macrocontribution.rb
@@ -1,7 +1,6 @@
 # require 'objectproperties' # Includes the class methods for the object_properties
 require 'local_store/source_record'
 require 'local_store/data_record'
-require 'local_store/macrocontribution_record'
 require 'local_store/workflow/workflow_record'
 require 'pagination/source_pagination'
 require 'query/source_query'
