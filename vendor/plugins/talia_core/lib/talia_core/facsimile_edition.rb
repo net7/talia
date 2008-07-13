18830a734ce07dd838d4bcf64046ca8fa783dd7a
danilo <danilo@2960d7dc-a92c-0410-a2fe-c68badc242c0>
2008-07-13 18:34:40 +0000

    Facsimile Editions: removed the no more useful simple mode image widget, added the image retrieval capability, by using the :format in the controller (left some commented-out code as a refernce for checking the usage of named routes), added search capability.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_core@757 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- vendor/plugins/talia_core/lib/talia_core/facsimile_edition.rb
+++ vendor/plugins/talia_core/lib/talia_core/facsimile_edition.rb
@@ -32,24 +32,92 @@ module TaliaCore
       #TODO: everything
       result =  ['N-IV-2,1','N-IV-2,2','N-IV-2,3', 'N-IV-2,4']
     end
+    
     # returns the description of the book given as parameter, taken from the "material description" contribution
     # which is supposed to be related to this Facsimile Edition
     def material_description(book)
       #TODO: everything
       result = "#{book} description"
     end
-    
-    # returns the URL of the small version of the image related to the given source.
-    # please note that, usually, source_uri is the uri of a book or page, while the image is 
-    # to be found in the facsimile related to it
-    # 
-    # special case: if source_uri is a book, the image should be taken from the facsimile 
-    # of the first page of that book (that also belong to the Macrocontribution)
-    
-    def small_image_url(source_uri)
+
+    # searches for the facsimile related to the page passed as a parameter
+    # which also belongs to the facsimile edition we're in.
+    # It then returns the first TaliaCore::ImageData object of it
+    def page_image_data(book, page, size)
       #TODO: everything
+      case size
+      when 'thumbnail'
+        fax = Source.find('egrepalysviola-1441')
+        fax.data('ImageData')[0]
+      else
+        fax = Source.find('egrepalysviola-1439')
+        fax.data('ImageData')[0]
+      end
+    end
+
+    # Starting from the book URI, it searches for the first of its pages belonging to the
+    # facsimile edition we're in and calls page_image to do the rest
+    def book_image_data(book, size)
+      #TODO: everything
+      #TODO: retrieve the first page being part of this facsimile edition from RDF
+      page = 'some magic tricks will fill this'
+      page_image_data(book, page, size)
     end
     
+    
+    # returns the copyright note of the facsimile related to the given page
+    # the copyright note is related to the archive where the material is kept
+    def copyright_note(page)
+      #TODO:everything
+      'copyright note'
+    end
+    
+
+    def search(requested_book='', requested_page='')
+      result = ''
+      case requested_page
+      when ''
+        case requested_book
+        when ''
+          #nothing was requested, nothing should be returned
+          raise ActiveRecord::RecordNotFound
+        else
+          # the book name has been passed, but we haven't the page name
+          # we redirect to the "page" action of the first page of the book
+          book = Source.find(requested_book) || nil
+#          this was used toghether with a different way of creating the redirection.
+#          please look at facsimile_editions_controller.rb 
+#          result = {:book => "#{book.id}"}
+          
+          result = [book.id]
+          #TODO: check that the book is part of the facsimile edition we're in
+        end
+      else 
+        # requested_page is not nil
+        case requested_book
+        when ''
+          #TODO: we said this case is senseless, we'll deal with it as if it was
+          # an error, but we should show a nice message
+          raise ActiveRecord::RecordNotFound
+        else
+          # both the book name and the page name were given, we redirect 
+          # the user right there, in the "page" action of it          
+          book = Source.find(requested_book)
+          qry1 = RdfQuery.new(:EXPRESSION, N::HYPER::part_of, book)
+          qry2 = RdfQuery.new(:EXPRESSION, N::HYPER::position_name, requested_page)
+          #TODO: check that the page is in the facsimile edition 
+          qry = RdfQuery.new(:AND, qry1, qry2)
+          page = qry.execute[0]
+#          this was used toghether with a different way of creating the redirection.
+#          please look at facsimile_editions_controller.rb 
+#          result = {:book => "#{book.id}", :page => "#{page.id}"}
+           
+          result = [book.id, page.id]
+        end
+      end
+      result
+    end
+  
     # returns the left or right "neighbour" source of the given source.
     # movement is expected in the form of "next" or "previous"
     # used, for instance, for browsing pages
@@ -57,7 +125,6 @@ module TaliaCore
       #TODO: everything
       result = 'N-IV-2,3'
     end
-
     
   end
 end
