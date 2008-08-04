0ed2e22c2e5d7c49ad39e5626cea755f9e7302e7
daniel <ghub@limitedcreativity.org>
2008-08-04 15:20:40 +0000

    Initializer autoloads model classes.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@843 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- config/initializers/talia.rb
+++ config/initializers/talia.rb
@@ -5,3 +5,11 @@ TaliaCore::SITE_NAME = 'DiscoverySource'
 
 TaliaCore::FACSIMILE_EDITION_PREFIX = 'facsimile_editions'
 TaliaCore::CRITICAL_EDITION_PREFIX = 'critical_editions'
+
+# Load all the internal model classes so that subclass-aware ActiveRecord
+# operations don't run into problems
+model_dir = File.join(RAILS_ROOT, 'app', 'models', 'talia_core')
+Dir.foreach(model_dir) do |file|
+  # Use the standard loader mechanism to avoid double-loading
+  TaliaCore.const_get(File.basename(file, '.rb').classify) if(File.extname(file) == '.rb')
+end
