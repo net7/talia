2086c3bd30d54848c9b49c12a24eeb41d2ad9624
danilo <danilo@2960d7dc-a92c-0410-a2fe-c68badc242c0>
2008-08-13 12:19:23 +0000

    added some critical edition files
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@894 2960d7dc-a92c-0410-a2fe-c68badc242c0

---
+++ app/views/widgets/critical_edition_menu/code/critical_edition_menu_widget.rb
@@ -0,0 +1,13 @@
+class CriticalEditionMenuWidget < Widgeon::Widget
+  
+  # This will run during the widget initialization. You can use all options
+  # for the widget as class variables, and all class variables that
+  # you set will be available as accessors in the template.
+  def on_init
+  end
+  
+  # Example callback for javascript
+  # callback :first_callback do |page|
+  #   page.insert_html(options)
+  # end
+end
