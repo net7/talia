f580f007ba4b366fde0c0c1404bd98ae067335a1
daniel <ghub@limitedcreativity.org>
2008-08-20 09:28:26 +0000

    IIP flash viewer integrated into Facsimile Edition
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@904 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- talia.sh
+++ talia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pd=`dirname "$0"`
 prog_dir=`cd $pd && pwd`
-jruby_dir="vendor/jruby-1.1.1"
+jruby_dir="vendor/jruby-1.1.3"
 
 export PATH=$prog_dir/$jruby_dir/bin:$prog_dir/$jruby_dir/lib/ruby/gems/1.8/bin:$PATH
 export PS1="talia-bash \w >> "
