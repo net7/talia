8d36a149afec82b154a8ee2c6852296304f83f8a
daniel <ghub@limitedcreativity.org>
2008-08-25 13:07:27 +0000

    Updates for IIP data
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@913 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- config/initializers/talia.rb
+++ config/initializers/talia.rb
@@ -1,4 +1,15 @@
 TaliaCore::Initializer.environment = ENV['RAILS_ENV']
-TaliaCore::Initializer.run("talia_core")
+TaliaCore::Initializer.run("talia_core") do |config|
+  # URI for the IIP server instance
+  config['iip_server_uri'] = 'http://localhost/fcgi-bin/iipsrv.fcgi'
+  # The external vips command that needs to be called to create the pyramid images
+  config['iip_command'] = '/opt/local/bin/vips'
+  # Thumbnail size for IIP thumbnails 
+  config['thumb_options'] = { :width => '128', :height => '128' }
+  # The root directory for the IIP-served images
+  # config['iip_root_directory_location'] = 'TALIA_ROOT/iip_root'
+  # The directory for "normal" data files
+  # config['data_directory_location'] = 'TALIA_ROOT/data'
+end
 
 TaliaCore::SITE_NAME = 'DiscoverySource'
