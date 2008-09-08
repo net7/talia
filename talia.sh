a7aa4d830e11f063c6c58eac1185963e0d36f53a
daniel <ghub@limitedcreativity.org>
2008-09-08 14:31:48 +0000

    simple config script
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@931 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- talia.sh
+++ talia.sh
@@ -4,6 +4,8 @@ prog_dir=`cd $pd && pwd`
 jruby_dir="vendor/jruby-1.1.3"
 
 export PATH=$prog_dir/$jruby_dir/bin:$prog_dir/$jruby_dir/lib/ruby/gems/1.8/bin:$PATH
+export PATH=$prog_dir/lib/scripts:$PATH
+export PATH=$prog_dir/vendor/plugins/talia_core/bin:$PATH
 export PS1="talia-bash \w >> "
 
 case $1 in
