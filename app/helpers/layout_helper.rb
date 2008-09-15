8f5a7af17b6ca83f547d7998cf0b612efe81b1aa
gluca <guidi.luca@gmail.com>
2008-09-15 12:29:40 +0000

    Types controller now uses application layout.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_app@952 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -12,24 +12,36 @@ module LayoutHelper
   end
   
   def sidebar
-    return if @hidden_sidebar
+    return if hidden_sidebar?
     sidebar_title = "#{@source.label} is" if @source
     @content_for_sidebar = widget :sidebar, 'active_tab' => 'context',
       'active_tab_options' => { :source => @source },
       'sidebar_title' => sidebar_title
   end
   
+  def types_sidebar
+    hide_sources_sidebar
+    @content_for_sidebar = widget :sidebar, 
+      'active_tab' => 'navigation',
+      'active_tab_options' => { :source_class => @type }
+  end
+  
   def hide_sidebar
     @hidden_sidebar = true
+  end
+  alias_method :hide_sources_sidebar, :hide_sidebar
+  
+  def hidden_sidebar?
+    !!@hidden_sidebar
   end
   
   def javascript(*file_names)
     @content_for_javascript ||= ""
-    @content_for_javascript << file_names.map { |fn| javascript_include_tag(fn.to_s) }.join
+    @content_for_javascript << file_names.map { |fn| javascript_include_tag(fn.to_s, :cache => true) }.join("\n")
   end
   
   def stylesheet(*file_names)
     @content_for_stylesheet ||= ""
-    @content_for_javascript << file_names.map { |fn| stylesheet_link_tag(fn.to_s) }.join
+    @content_for_stylesheet << file_names.map { |fn| stylesheet_link_tag(fn.to_s, :cache => true) }.join("\n")
   end
 end
--- app/helpers/types_helper.rb
+++ app/helpers/types_helper.rb
@@ -1,2 +1,11 @@
 module TypesHelper
+  def types_javascripts
+    javascript :mainElementPositioning, :toolbar,
+      :window_onresize_event, :window_onload_event, :text_onresize_event,
+      :handleSearchField, :utilities
+  end
+  
+  def types_stylesheets
+    stylesheet :pod_style_navigation, :wizard
+  end
 end
