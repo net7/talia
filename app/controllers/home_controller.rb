37c81f5770de4ee2204e824ad71d4b2fbe36f663
daniel <ghub@limitedcreativity.org>
2008-09-19 17:19:07 +0000

    Activated Language selection links
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@974 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-   
+     
   def start
 
     @facsimile_editions = TaliaCore::FacsimileEdition.find(:all)
@@ -7,5 +7,18 @@ class HomeController < ApplicationController
     
     @page_title = "Welcome to #{TaliaCore::SITE_NAME}".t
   end
+  
+  def select_language
+    if(allowed_locales.include?(params['id']))
+      Locale.set(params['id'])
+      redirect_to :action => 'start'
+    end
+  end
 
+  private
+  
+  def allowed_locales
+    %w(en-US de-DE fr-FR it-IT)
+  end
+  
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,7 @@
 module HomeHelper
+  
+  def select_language(name, locale, accesskey)
+    link_to(name, { :action => 'select_language', :id => locale }, { :title => "#{name}, AccessKey: #{accesskey}", :accesskey => accesskey })
+  end
+  
 end
