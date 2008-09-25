a0c54f223a6cfd953a53e89b50b7dd7aa4175af1
daniel <ghub@limitedcreativity.org>
2008-09-25 09:07:29 +0000

    Minor update.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_core@983 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- vendor/plugins/talia_core/lib/talia_util/configuration/mysql_database_setup.rb
+++ vendor/plugins/talia_core/lib/talia_util/configuration/mysql_database_setup.rb
@@ -93,7 +93,7 @@ module TaliaUtil
           :username => @root_user,
           :password => @root_pw,
           :database => database,
-          :sock => @sock || '/tmp/mysql.sock'
+          :socket => @sock || '/tmp/mysql.sock'
         }
       end
       
