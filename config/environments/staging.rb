71f5437c4fb58f2744ced3ace4328dfc4d45a5a3
gluca <guidi.luca@gmail.com>
2008-10-27 09:13:35 +0000

    Added asset_host directive to Staging environment
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_app@1077 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,3 +1,6 @@
 config.cache_classes = true
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = true
+# Make sure to add the proper value for your host, i.e. talia.local could be replaced with localhost.
+# Then make sure to add assets0 to assets3 to your /etc/hosts and flush the DNS cache of your Operating System
+config.action_controller.asset_host                  = "http://assets%d.talia.local"
