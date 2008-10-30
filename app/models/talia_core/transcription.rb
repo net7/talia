e97c433a86458eba01a380aa15012dddaebb5f3d
danilo <danilo@2960d7dc-a92c-0410-a2fe-c68badc242c0>
2008-10-30 11:43:56 +0000

    added a very simple Exception handling for XML transformations in text_reconstructions and transcriptions to_html methods.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@1100 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- app/models/talia_core/transcription.rb
+++ app/models/talia_core/transcription.rb
@@ -25,34 +25,40 @@ module TaliaCore
       saxon = JXslt::Saxon.new
       infile = self.data[0].file_path
       output = ''
-      case self.dcns::format.first
-      when 'application/xml+hnml'
-        case version
-        when 'diplomatic'
-          xsl1 = 'transcription_diplomatic.xsl'
-          xsl2 = 'transcription_diplomatic_2.xsl'
-        when 'linear'   
-          xsl1 = 'transcription_linear.xsl'
-          xsl2 = 'transcription_linear_2.xsl'
+      begin
+        case self.dcns::format.first
+        when 'application/xml+hnml'
+          case version
+          when 'diplomatic'
+            xsl1 = 'transcription_diplomatic.xsl'
+            xsl2 = 'transcription_diplomatic_2.xsl'
+          when 'linear'   
+            xsl1 = 'transcription_linear.xsl'
+            xsl2 = 'transcription_linear_2.xsl'
+          end
+          max_layer = hnml_max_layer
+          middle_output = ''       
+          if max_layer != '' 
+            shown_layer = layer.nil? ? max_layer : layer
+            transformer_parameters = {'layer' => shown_layer}
+          end
+          xsl = 'public/xsl/hnml/' + xsl1
+          middle_output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})
+          xsl = 'public/xsl/hnml/' + xsl2 
+          output = saxon.transform(xsl, middle_output, nil, options = {:in => "string", :out => "string", :transformer_parameters => transformer_parameters})
+        when 'application/xml+tei'
+          xsl = 'public/xsl/TEI/p4/html/tei.xsl'
+          output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string"})
+        when 'application/xml+wit_tei'
+          xsl = 'public/xsl/WitTEI/wab-transform.xsl'
+          # visning is the parameter for the version in the wab-transform.xsl file
+          transformer_parameters = {'visning' => version}
+          output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})
         end
-        max_layer = hnml_max_layer
-        middle_output = ''       
-        if max_layer != '' 
-          shown_layer = layer.nil? ? max_layer : layer
-          transformer_parameters = {'layer' => shown_layer}
-        end
-        xsl = 'public/xsl/hnml/' + xsl1
-        middle_output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})
-        xsl = 'public/xsl/hnml/' + xsl2 
-        output = saxon.transform(xsl, middle_output, nil, options = {:in => "string", :out => "string", :transformer_parameters => transformer_parameters})
-      when 'application/xml+tei'
-        xsl = 'public/xsl/TEI/p4/html/tei.xsl'
-        output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string"})
-      when 'application/xml+wit_tei'
-        xsl = 'public/xsl/WitTEI/wab-transform.xsl'
-        # visning is the parameter for the version in the wab-transform.xsl file
-        transformer_parameters = {'visning' => version }
-        output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})
+      rescue #TODO: handle these specific (java) exception: 
+        #   net.sf.saxon.trans.XPathException
+        #    org.xml.sax.SAXParseException
+        output = "XML is Broken!"
       end
       output
     end
