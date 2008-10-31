b684cb06fc224542948d793a663e8421c36864cd
gluca <guidi.luca@gmail.com>
2008-10-31 14:15:05 +0000

    Make sure of manually pass URI location for import_controller.rb
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@1107 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -9,7 +9,7 @@ class ImportController < ApplicationController
         begin
           # TODO should we move the xml doc creation in #import?
           @document = TaliaUtil::HyperImporter::Importer.import(REXML::Document.new(params[:document]))
-          render :inline => 'The source has been created.', :status => :created, :location => @document
+          render :inline => 'The source has been created.', :status => :created, :location => @document.uri.to_s
         rescue Exception => e
           render :xml => e.message, :status => :unprocessable_entity
         end
