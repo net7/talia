468b40014daa2d1883fad68831d257ed53e9edf7
danilo <danilo@2960d7dc-a92c-0410-a2fe-c68badc242c0>
2008-11-11 12:13:41 +0000

    workaround for missing files
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@1198 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- app/models/talia_core/text_reconstruction.rb
+++ app/models/talia_core/text_reconstruction.rb
@@ -17,47 +17,52 @@ module TaliaCore
       when 'text/html'
         ['standard']
       else
-        raise(ArgumentError, "Unknown content type for #{self.uri}: #{self.dcns::format.first}")
+        #FIXME: add a better thing for this after the review        raise(ArgumentError, "Unknown content type for #{self.uri}: #{self.dcns::format.first}")
+        nil
       end
     end
     
     def to_html(version=nil, layer=nil)
       # if no version is specified, it takes the first available
+      return '' if available_versions.nil?
       version = available_versions[0] if version.nil?
       require 'JXslt/jxslt'
       saxon = JXslt::Saxon.new
       infile = self.data[0].file_path
       output = ''
-      begin
-        case self.dcns::format.first
-        when 'application/xml+hnml'
-          max_layer = hnml_max_layer
-          middle_output = ''     
-          if max_layer != '' 
-            shown_layer = layer.nil? ? max_layer : layer
-            transformer_parameters = {'layer' => shown_layer}
+      if File.exist?(infile)
+        begin
+          case self.dcns::format.first
+          when 'application/xml+hnml'
+            max_layer = hnml_max_layer
+            middle_output = ''     
+            if max_layer != '' 
+              shown_layer = layer.nil? ? max_layer : layer
+              transformer_parameters = {'layer' => shown_layer}
+            end
+            xsl = 'public/xsl/hnml/edition_linear.xsl'
+            middle_output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})
+            xsl = 'public/xsl/hnml/edition_linear_2.xsl'
+            output = saxon.transform(xsl, middle_output, nil, options = {:in => "string", :out => "string", :transformer_parameters => transformer_parameters})
+          when 'application/xml+tei', 'application/xml+tei-p4', 'application/xml+tei-p5'
+            xsl = 'public/xsl/TEI/p4/html/tei.xsl'
+            output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string"})
+          when 'application/xml+wit_tei'
+            xsl = 'public/xsl/WitTEI/wab-transform.xsl'    
+            # visning is the parameter for the version in the wab-transform.xsl file        
+            transformer_parameters = {'visning' => version}
+            output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})          
+          when 'text/html'
+            xsl = 'public/xsl/plain/plain.xsl'
+            output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})          
           end
-          xsl = 'public/xsl/hnml/edition_linear.xsl'
-          middle_output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})
-          xsl = 'public/xsl/hnml/edition_linear_2.xsl'
-          output = saxon.transform(xsl, middle_output, nil, options = {:in => "string", :out => "string", :transformer_parameters => transformer_parameters})
-        when 'application/xml+tei', 'application/xml+tei-p4', 'application/xml+tei-p5'
-          xsl = 'public/xsl/TEI/p4/html/tei.xsl'
-          output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string"})
-        when 'application/xml+wit_tei'
-          xsl = 'public/xsl/WitTEI/wab-transform.xsl'    
-          # visning is the parameter for the version in the wab-transform.xsl file        
-          transformer_parameters = {'visning' => version}
-          output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})          
-        when 'text/html'
-          file = File.open(infile, 'r')
-          output = file.read
-          file.close if file
+        rescue #TODO: handle these specific (java) exception: 
+          #   net.sf.saxon.trans.XPathException
+          #    org.xml.sax.SAXParseException
+          output = "XML is Broken!"
         end
-      rescue #TODO: handle these specific (java) exception: 
-        #   net.sf.saxon.trans.XPathException
-        #    org.xml.sax.SAXParseException
-        output = "XML is Broken!"
+      else
+        puts "Warning file was missing: #{infile} calculation will continue"  
       end
       output
     end
--- app/models/talia_core/transcription.rb
+++ app/models/talia_core/transcription.rb
@@ -15,56 +15,59 @@ module TaliaCore
       when 'text/html'
         ['standard']
       else
-        raise(ArgumentError, "Unknown content type for#{self.uri}: #{self.dcns::format.first}")
+        #FIXME: add a better thing for this after the review        raise(ArgumentError, "Unknown content type for #{self.uri}: #{self.dcns::format.first}")
+        nil
       end
     end
     
     
     def to_html(version=nil, layer=nil)
       # if no version is specified, it takes the first available
+      return '' if available_versions.nil?
       version = available_versions[0] if version.nil?
       require 'JXslt/jxslt'
       saxon = JXslt::Saxon.new
       infile = self.data[0].file_path
       output = ''
-      begin
-        case self.dcns::format.first
-        when 'application/xml+hnml'
-          case version
-          when 'diplomatic'
-            xsl1 = 'transcription_diplomatic.xsl'
-            xsl2 = 'transcription_diplomatic_2.xsl'
-          when 'linear'   
-            xsl1 = 'transcription_linear.xsl'
-            xsl2 = 'transcription_linear_2.xsl'
+      if File.exist?(infile) 
+        begin
+          case self.dcns::format.first
+          when 'application/xml+hnml'
+            case version
+            when 'diplomatic'
+              xsl1 = 'transcription_diplomatic.xsl'
+              xsl2 = 'transcription_diplomatic_2.xsl'
+            when 'linear'   
+              xsl1 = 'transcription_linear.xsl'
+              xsl2 = 'transcription_linear_2.xsl'
+            end
+            max_layer = hnml_max_layer
+            middle_output = ''       
+            if max_layer != '' 
+              shown_layer = layer.nil? ? max_layer : layer
+              transformer_parameters = {'layer' => shown_layer}
+            end
+            xsl = 'public/xsl/hnml/' + xsl1
+            middle_output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})
+            xsl = 'public/xsl/hnml/' + xsl2 
+            output = saxon.transform(xsl, middle_output, nil, options = {:in => "string", :out => "string", :transformer_parameters => transformer_parameters})
+          when 'application/xml+tei', 'application/xml+tei-p4', 'application/xml+tei-p5'
+            xsl = 'public/xsl/TEI/p4/html/tei.xsl'
+            output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string"})
+          when 'application/xml+wit_tei'
+            xsl = 'public/xsl/WitTEI/wab-transform.xsl'
+            # visning is the parameter for the version in the wab-transform.xsl file
+            transformer_parameters = {'visning' => version}
+            output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})
+          when 'text/html'
+            xsl = 'public/xsl/plain/plain.xsl'
+            output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})          
           end
-          max_layer = hnml_max_layer
-          middle_output = ''       
-          if max_layer != '' 
-            shown_layer = layer.nil? ? max_layer : layer
-            transformer_parameters = {'layer' => shown_layer}
-          end
-          xsl = 'public/xsl/hnml/' + xsl1
-          middle_output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})
-          xsl = 'public/xsl/hnml/' + xsl2 
-          output = saxon.transform(xsl, middle_output, nil, options = {:in => "string", :out => "string", :transformer_parameters => transformer_parameters})
-        when 'application/xml+tei', 'application/xml+tei-p4', 'application/xml+tei-p5'
-          xsl = 'public/xsl/TEI/p4/html/tei.xsl'
-          output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string"})
-        when 'application/xml+wit_tei'
-          xsl = 'public/xsl/WitTEI/wab-transform.xsl'
-          # visning is the parameter for the version in the wab-transform.xsl file
-          transformer_parameters = {'visning' => version}
-          output = saxon.transform(xsl, infile, nil, options = {:in => "stream", :out => "string", :transformer_parameters => transformer_parameters})
-        when 'text/html'
-          file = File.open(infile, 'r')
-          output = file.read
-          file.close if file
+        rescue #TODO: handle these specific (java) exception: 
+          #   net.sf.saxon.trans.XPathException
+          #    org.xml.sax.SAXParseException
+          output = "XML is Broken!"
         end
-      rescue #TODO: handle these specific (java) exception: 
-        #   net.sf.saxon.trans.XPathException
-        #    org.xml.sax.SAXParseException
-        output = "XML is Broken!"
       end
       output
     end
