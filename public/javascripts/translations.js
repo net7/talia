60d2032da2f7cefbfb1eb9eb516c661c0dd89e37
gluca <guidi.luca@gmail.com>
2008-11-27 15:42:05 +0000

    Forgotten file
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_app@1220 2960d7dc-a92c-0410-a2fe-c68badc242c0

---
+++ public/javascripts/translations.js
@@ -0,0 +1,43 @@
+var TranslationFetcher = Class.create({
+  initialize: function(selectId, tableId){
+    this.url = "/admin/translations/search";
+    this.selectElement = $(selectId);
+    this.tableRows = $$('#'+tableId+' tr');
+    this.tableRows.shift(); // remove the row contains th
+    // I didn't used #observe because it was double firing #loadTranslations
+    this.selectElement.onchange = this.loadTranslations.bindAsEventListener(this);
+  },
+
+  loadTranslations: function(){
+    keys = this.tableRows.map(function(row, i){
+      key = row.select("td:nth-child(1) input").first().value;
+      return "key"+i+"="+encodeURIComponent(key);
+    }).join('&');
+    locale = encodeURIComponent(this.selectElement.value);
+    new Ajax.Request(this.url + "?locale="+ locale +"&"+ keys, {
+      method: 'get',
+      onSuccess: function(transport){
+        translations = $H(transport.responseText.evalJSON());
+        this.updateTableWithTranslations($H(translations));
+      }.bind(this)
+    });
+  },
+  updateTableWithTranslations: function(translations){
+    $$('.reference_translation').each(function(element){element.remove();});
+    this.tableRows.each(function(row){
+      key = row.select("td:nth-child(1) input").first().value;
+      translation = translations.get(key);
+      tr = document.createElement('tr');
+      tr.addClassName('reference_translation');
+      td = document.createElement('td');
+      translationTd = document.createElement('td');
+      translationTd.update(translation);
+      tr.insert(td, {position:'top'});
+      tr.insert(translationTd, {position:'bottom'});
+
+      row.insert(tr,{position:'after'});
+    });
+  }
+});
+
+new TranslationFetcher('reference_languages', 'translations');
