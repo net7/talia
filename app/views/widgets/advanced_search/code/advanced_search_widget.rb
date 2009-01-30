94add79d1bf6f4f92d5c2ee70ac45c1de6d3accd
tofani <robertox2k@gmail.com>
2009-01-30 15:30:37 +0000

    Improvement for advanced search in critical edition. Now it can reload previous submitted values. (In next commit it work also in sophia vision)
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@1344 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- app/views/widgets/advanced_search/code/advanced_search_widget.rb
+++ app/views/widgets/advanced_search/code/advanced_search_widget.rb
@@ -24,6 +24,9 @@ class AdvancedSearchWidget < Widgeon::Widget
       # reset current size value
       widget_session[:current_size] = 0
       
+      # set search mode
+      widget_session[:mode] = @options[:mode]
+
       # store label for all field
       widget_session[:field_1_label] = @options[:field_1_label] || 'work'
       widget_session[:field_2_label] = @options[:field_2_label] || 'aphorisms'
@@ -53,6 +56,205 @@ class AdvancedSearchWidget < Widgeon::Widget
     end
   end
 
+  # return an hidden field tag for search type
+  def tag_search_type
+    if is_avmedia_search
+      return hidden_field_tag 'search_type', 'media'
+    else
+      return hidden_field_tag 'search_type', 'mc'
+    end
+  end
+
+  # return an input tag for words
+  def tag_word
+    if is_avmedia_search
+      raise("Tag not supported in avmedia search")
+    else
+      return "<label>#{t(:'talia.search.search_phrase_prompt')}</label> <input type='text' name='words' value='#{params[:words]}' />"
+    end
+  end
+
+  def tag_any_all
+    if is_avmedia_search
+      raise("Tag not supported in avmedia search")
+    else
+      # load previous operator used
+      case params[:operator]
+      when 'or'
+        tag_to_check = 'or'
+      when 'and'
+        tag_to_check = 'and'
+      else
+        tag_to_check = 'or'
+      end
+
+      # create tags
+      tag_string = "<input #{"checked" if (tag_to_check == 'or')} type='radio' name='operator' value='or' />"
+      tag_string << "<label class='toradio'> #{t(:'talia.search.any')}</label>"
+      tag_string << "<input #{"checked" if (tag_to_check == 'and')} type='radio' name='operator' value='and' />"
+      tag_string << "<label class='toradio'> #{t(:'talia.search.all')}</label>"
+
+      return tag_string
+    end
+  end
+
+  # return a select tag for work
+  def tag_work
+    if is_avmedia_search
+      raise("Tag not supported in avmedia search")
+    else
+      # create tags
+      tag_string = "<label>#{t(:"talia.search.#{widget_session[:field_1_label]}")} </label>"
+      tag_string << "<select id=\"src_line_#{widget_session[:current_size]}_field_1\" onchange=\"#{onchange_link(widget_session[:current_size])}\" name=\"mc[]\" >"
+
+      works.each do |item|
+        unless params[:mc].nil?
+          if(params[:mc][widget_session[:current_size]-1] == item.uri.to_s)
+            selected = true
+          end
+        end
+
+        tag_string << "<option #{"selected" if selected} value=\"#{item.uri}\">#{item.title}</option>"
+      end
+
+      tag_string << "</select>"
+
+      return tag_string
+    end
+  end
+
+  # return a select tag for aphorisms
+  def tag_aphorisms
+    if is_avmedia_search
+      raise("Tag not supported in avmedia search")
+    else
+      # load previous mc_from used
+      if params[:mc_from].nil?
+        selected_value = :first
+        selected_mc = works.first.uri
+      else
+        selected_value = params[:mc_from][widget_session[:current_size]-1]
+        selected_mc = params[:mc][widget_session[:current_size]-1]
+      end
+
+      # create tags
+      tag_string = "<label>#{t(:"talia.search.#{widget_session[:field_2_label]}")} </label>"
+      tag_string << w.partial(:advanced_search_select,
+        :locals => {:field_name=> 'mc_from[]',
+          :current_size => widget_session[:current_size],
+          :selected_value => selected_value,
+          :data => subparts(selected_mc)})
+
+      return tag_string
+    end
+  end
+
+  # return a select tag for through
+  def tag_through
+    if is_avmedia_search
+      raise("Tag not supported in avmedia search")
+    else
+      # load previous mc_from used
+      if params[:mc_to].nil?
+        selected_value = :last
+        selected_mc = works.first.uri
+      else
+        selected_value = params[:mc_to][widget_session[:current_size]-1]
+        selected_mc = params[:mc][widget_session[:current_size]-1]
+      end
+
+      # create tags
+      tag_string = "<label>#{t(:"talia.search.#{widget_session[:field_2_label]}")} </label>"
+      tag_string << w.partial(:advanced_search_select,
+        :locals => {:field_name=> 'mc_to[]',
+          :current_size => widget_session[:current_size],
+          :selected_value => selected_value,
+          :data => subparts(selected_mc)})
+
+      return tag_string
+    end
+  end
+
+  # return a generic select tag with an element selected
+  def tag_search_select(field_name, selected_value, data = nil)
+    if is_avmedia_search
+      raise("Tag not supported in avmedia search")
+    else
+      tag_string = "<select name='#{field_name}' id='#{"src_line_#{widget_session[:current_size]}_#{field_name}"}' >"
+
+      unless data.nil?
+        data.each_with_index do |item,index|
+          case selected_value
+          when :first
+            is_selected = true if index == 0
+          when :last
+            is_selected = true if index == (data.size - 1)
+          when item[0]
+            is_selected = true
+          end
+
+          tag_string << "<option #{"selected" if is_selected} value=#{item[0]}>#{item[1]}</option>"
+        end
+      end
+
+      tag_string << "</select>"
+
+      return tag_string
+    end
+  end
+
+  # return a select tag for avmedia
+  def tag_avmedia_select
+    if !is_avmedia_search
+      raise("Tag supported only in avmedia search")
+    else
+      tag_string = "<select id=\"#{ "src_line_#{widget_session[:current_size]}_field" }\" onchange=\"#{ onchange_link(widget_session[:current_size]) }\">"
+      tag_string << "<option value=\"title\" selected=\"selected\">#{t(:'talia.search.title')}</option>"
+      tag_string << "<option value=\"abstract\">#{t(:'talia.search.abstract') }</option>"
+      tag_string << "<option value=\"keyword\">#{t(:'talia.search.keyword')}</option>"
+      tag_string << "</select>"
+
+      return tag_string
+    end
+  end
+
+  # return an input tag for avmedia title words
+  def tag_avmedia_title_words
+    if !is_avmedia_search
+      raise("Tag supported only in avmedia search")
+    else
+      tag_string = "<input type=\"text\" name=\"title_words[]\" id=\"#{"src_line_#{widget_session[:current_size]}_field_value"}>\" />"
+      
+      return tag_string
+    end
+  end
+
+  # return an input tag for avmedia abstract words
+  def tag_avmedia_abstract_words
+    if !is_avmedia_search
+      raise("Tag supported only in avmedia search")
+    else
+      tag_string = "<input type=\"text\" name=\"abstract_words[]\" id=\"src_line_#{widget_session[:current_size]}_field_value\" />"
+
+      return tag_string
+    end
+  end
+
+  # return an input tag for avmedia keywords
+  def tag_avmedia_keywords
+    if !is_avmedia_search
+      raise("Tag supported only in avmedia search")
+    else
+      tag_string = "<select name=\"keywords[]\" id=\"src_line_#{widget_session[:current_size]}_field_value\">"
+      keyword.each do |item|
+        tag_string << "<option value='#{item}'>#{item}</option>"
+      end
+      tag_string << "</select>"
+    end
+
+    return tag_string
+  end
+
   # Return a minus button
   def minus_link(current_size)
     if current_size > 0
@@ -73,8 +275,8 @@ class AdvancedSearchWidget < Widgeon::Widget
   end
   
   # return onchange link for work field
-  def onchange_link(current_size, version="default")
-    if version == "default"
+  def onchange_link(current_size)
+    if !is_avmedia_search
       remote_function(:with => "'src_line_#{current_size}_field_1_value=' + $F('src_line_#{current_size}_field_1')",
         :url => {:controller => "widgeon", 
           :action => "callback", 
@@ -86,7 +288,7 @@ class AdvancedSearchWidget < Widgeon::Widget
       remote_function(:with => "'src_line_#{current_size}_field=' + $F('src_line_#{current_size}_field')",
         :url => {:controller => "widgeon", 
           :action => "callback",
-          :call_options =>  WidgeonEncoding.encode_options({:javascript => 'retrieve_keyword_content', 
+          :call_options =>  WidgeonEncoding.encode_options({:javascript => 'retrieve_avmedia_content',
               :current_size => current_size, 
               :widget_class => self.class.widget_name,
               :widget_id => self.widget_id})})      
@@ -126,39 +328,43 @@ class AdvancedSearchWidget < Widgeon::Widget
       item.keyword_value
     end
   end
-  
-  # callback for plus button
-  callback :add_work_line do |page|
-    # check if current_size is present
-    raise(ArgumentError, "Required argument missing") unless(@current_size)
 
+  def add_work_line
     # increment current_size
     widget_session[:current_size] += 1
 
-    
     # add new row to advanced search
-    page.insert_html :before, 
-      'src_line_tail', 
-      partial(:advanced_search_work, :locals => {
+    partial(:advanced_search_work, :locals => {
         :current_size => widget_session[:current_size],
         :field_1_label => widget_session[:field_1_label],
         :field_2_label => widget_session[:field_2_label],
         :field_3_label => widget_session[:field_3_label]
-      })
+      })    
   end
-  
-  # callback for plus button for AvMedia
-  callback :add_avmedia_generic_row do |page|
-    # check if current_size is present
-    raise(ArgumentError, "Required argument missing") unless(@current_size)
 
+  def add_avmedia_generic_row
     # increment current_size
     widget_session[:current_size] += 1
 
     # add new row to advanced search
+    partial(:advanced_search_avmedia_row, :locals => {:current_size => widget_session[:current_size], :keyword_list => keyword})
+  end
+  
+  # callback for plus button
+  callback :add_work_line do |page|
+
+    page.insert_html :before,
+      'src_line_tail',
+      self.add_work_line
+
+  end
+  
+  # callback for plus button for AvMedia
+  callback :add_avmedia_generic_row do |page|
+    # add new row to advanced search
     page.insert_html :before, 
       'src_line_tail', 
-      partial(:advanced_search_avmedia_row, :locals => {:current_size => widget_session[:current_size], :keyword_list => keyword})
+      self.add_avmedia_generic_row
   end
   
   # callback for minus button
@@ -208,24 +414,24 @@ class AdvancedSearchWidget < Widgeon::Widget
     # check if current_size is present 
     raise(ArgumentError, "Required argument missing") unless(@current_size)
 
-    # if parameter is keyword, replace it with combobox
+    # replace current field tag with new field tag
     new_object = ""
     case params["src_line_#{@current_size}_field"]
     when "title"
-      new_object << "<input type='text' name='title_words[]' id='src_line_#{current_size}_field_value' />"
+      new_object << tag_avmedia_title_words
     when "abstract"
-      new_object << "<input type='text' name='abstract_words[]' id='src_line_#{current_size}_field_value' />"
+      new_object << tag_avmedia_abstract_words
     when "keyword"
-      new_object << "<select name='keywords[]' id='src_line_#{current_size}_field_value'>"
-      keyword.each do |item|
-        new_object << "<option value='#{item}'>#{item}</option>"
-      end
-      new_object << "</select>"
+      new_object << tag_avmedia_keywords
     end
         
-    # replace textbox with combobox
+    # replace field
     page.replace "src_line_#{current_size}_field_value", new_object
 
+  end
+
+  def is_avmedia_search
+    return (widget_session[:mode] == :avmedia)
   end
   
 end
