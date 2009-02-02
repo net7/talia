53d0bb74559f20bed95aa3a30c9e6b90e6add8a5
gluca <guidi.luca@gmail.com>
2009-02-02 15:39:48 +0000

    Sophiavision: fixed uri handling for AvMediaSourcesController
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@1359 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- app/controllers/av_media_sources_controller.rb
+++ app/controllers/av_media_sources_controller.rb
@@ -1,13 +1,21 @@
 class AvMediaSourcesController < ApplicationController
-  
+  before_filter :normalize_uri, :only => :show
+
   def show
-    element_uri = N::LOCAL + 'av_media_sources/' + params[:id]
-    raise(ArgumentError, "Unknow Media Source #{element_uri}") unless(TaliaCore::AvMedia.exists?(element_uri))
+    # TODO theoretically the following statement isn't needed and also performs a useless db query,
+    # because AR::Base#find raise a proper exception if something is missing.
+    raise(ArgumentError, "Unknow Media Source #{params[:id]}") unless(TaliaCore::AvMedia.exists?(params[:id]))
     
-    @element = TaliaCore::AvMedia.find(element_uri)
+    @element = TaliaCore::AvMedia.find(params[:id])
     cat = @element.category
     @path = [ { :text => t(:"talia.names.#{cat.class.name.underscore}.#{cat.name.underscore}"), :link => ("#{N::LOCAL}categories/" + cat.uri.local_name) }, { :text => @element.title } ]
     @title = t(:'talia.global.category') + " | #{cat.name}"
     @subtitle = t(:'talia.global.category')
   end
+  
+  private
+    def normalize_uri
+      params[:id] = CGI::unescape(params[:id]).gsub(' ', '+')
+      params[:id] = N::LOCAL + 'av_media_sources/' + params[:id]
+    end
 end
