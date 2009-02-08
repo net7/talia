cf8427ca362e3476d2fc473c6cb6cfe3e3cdc804
daniel <ghub@limitedcreativity.org>
2009-02-08 22:49:40 +0000

    Remove Source modification backend for now.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@1403 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,6 @@ class AdminController < ApplicationController
   require_role 'admin'
 
   def index
-    @links = %w(users sources translations)
+    @links = %w(users translations) # sources removed as non-working
   end
 end
