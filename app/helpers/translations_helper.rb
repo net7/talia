f512130312cb8dd3c2e589df4e03d7a205526fcf
gluca <guidi.luca@gmail.com>
2009-02-16 12:39:42 +0000

    Introducing InPlaceTranslator as cheap version of Click to Globalize
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@1414 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- app/helpers/translations_helper.rb
+++ app/helpers/translations_helper.rb
@@ -5,7 +5,7 @@ module TranslationsHelper
     result = content_tag(:span, '&nbsp;', :id => "#{symbol.to_s}_activator", :class => 'translate_element')
     result << content_tag(:span, symbol.t, :id => symbol.to_s)
     result << content_tag(:script, :type => 'text/javascript') do
-      "new Ajax.InPlaceEditor('#{symbol.to_s}', '#{url_for(:controller => 'locale', :action => 'translate')}', { externalControl:'#{symbol.to_s}_activator', externalControlOnly:true, callback: function(form, value) { return 'key=#{symbol.to_s}&value='+escape(value) } });"
+      "new InPlaceTranslator('#{symbol}');"
     end
   end
   alias_method :t, :translate
---
+++ public/javascripts/in_place_translator.js
@@ -0,0 +1,58 @@
+// InPlaceTranslator is a lightweight version of Click to Globalize
+Ajax.InPlaceEditor.prototype = Object.extend(Ajax.InPlaceEditor.prototype,{
+  createHiddenField: function(){
+    var textField = document.createElement("input");
+    textField.obj = this;
+    textField.type = 'hidden';
+    textField.name = 'key';
+    textField.value = this.options.hiddenValue;
+    var size = this.options.size || this.options.cols || 0;
+    if (size != 0) textField.size = size;
+    this._form.appendChild(textField);
+  }
+});
+
+// Fix for: http://dev.rubyonrails.org/ticket/4579
+Ajax.InPlaceEditor.prototype.initialize = Ajax.InPlaceEditor.prototype.initialize.wrap(
+	function(proceed, element, url, options) {
+		element = $(element);
+    if($w('TD TH').include(element.tagName)){
+			element.observe('click',     this.enterEditMode.bindAsEventListener(this));
+			element.observe('mouseover', this.enterHover.bindAsEventListener(this));
+			element.observe('mouseout',  this.leaveHover.bindAsEventListener(this));
+      element.innerHTML = "<span>" + element.textContent + "</span>";
+      element = element.down();
+    }
+		proceed(element, url, options);
+	}
+);
+
+Ajax.InPlaceEditor.prototype.createForm = Ajax.InPlaceEditor.prototype.createForm.wrap(
+  function(proceed) {
+    proceed();
+    this.createHiddenField();
+  }
+);
+
+var InPlaceTranslator = Class.create({
+  initialize: function(id){
+    this.id = id;
+    this.url = '/locale/translate';
+    this.parentElement = $(this.id).up();
+    this.parentElement.observe('click', function(event){
+      if(event.element().tagName == 'A') {event.stop(); return;}
+      this.editor.handleFormSubmission(event);
+    }.bind(this));
+    this.editor = this.createInPlaceEditor();
+  },
+  createInPlaceEditor: function(){
+    return new Ajax.InPlaceEditor(this.id, this.url, {
+      hiddenValue: this.id, // this.id should be the translation key too
+      externalControl: this.id + '_activator',
+      externalControlOnly: true,
+      callback: function(form, value) {
+        return 'key='+escape(this.id)+'&value='+escape(value);
+      }.bind(this)
+    });
+  }
+});
