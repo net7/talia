6f79a04c325f307a8552bddd527931e6a295f397
gluca <guidi.luca@gmail.com>
2009-02-18 10:28:45 +0000

    Introducing staging environment for cache testing
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/discovery_app@1418 2960d7dc-a92c-0410-a2fe-c68badc242c0

---
+++ config/environments/staging.rb
@@ -0,0 +1,12 @@
+config.cache_classes = true
+
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = true
+
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs                         = true
+config.action_controller.perform_caching             = true
+
+# Don't care if the mailer can't send
+config.action_mailer.raise_delivery_errors = false
