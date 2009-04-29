842bd04dbfa14a0d1447086c96ecc2f3ead7852b
tofani <robertox2k@gmail.com>
2009-04-29 10:13:54 +0200

    Now the advanced search can show "All occurrence" link for return to original search.

--- app/controllers/critical_editions_controller.rb
+++ app/controllers/critical_editions_controller.rb
@@ -74,7 +74,7 @@ class CriticalEditionsController < SimpleEditionController
       @words = params[:words]
             
       # get chosen_book for menu
-      if params[:mc_single]
+      if ((params[:mc_single]) && (params[:mc_single] != ''))
         @source = TaliaCore::Source.find(params[:mc_single])
         case @source
         when TaliaCore::Book
