1ec18ef7f2ee4ef840396f738f82d457995f5217
Luca Guidi <guidi.luca@gmail.com>
2009-06-19 13:55:55 +0200

    Don't load the tag cloud when show a single keyword. This enhance usability and performances.

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,22 +1,21 @@
 class KeywordsController < ApplicationController
 
   layout 'simple_page'
-  
+
+  def index
+    @path     = [{ :text => 'Keywords' }]
+    @keys     = TaliaCore::Keyword.find(:all)
+    @title    = t(:'talia.global.keywords')
+    @subtitle = t(:'talia.global.keywords')
+  end
+
   def show
     element_uri = N::LOCAL + 'keywords/' + params[:id]
 
-    @element = TaliaCore::Keyword.find(element_uri)
-    @keys = TaliaCore::Keyword.find(:all)
-    val = t(:"talia.keywords.#{@element.keyword_value}")
-    @path = [ { :text => t(:'talia.global.keyword'), :link => '/keywords/' }, { :text => val } ]
-    @title = t(:'talia.global.keyword') + " | #{val}"
+    @element  = TaliaCore::Keyword.find(element_uri)
+    val       = t(:"talia.keywords.#{@element.keyword_value}")
+    @path     = [ { :text => t(:'talia.global.keyword'), :link => '/keywords/' }, { :text => val } ]
+    @title    = t(:'talia.global.keyword') + " | #{val}"
     @subtitle = t(:'talia.global.keyword')
-  end
-
-  def index
-    @path = [ { :text => 'Keywords'}]
-    @keys = TaliaCore::Keyword.find(:all)
-    @title = t(:'talia.global.keywords')
-    @subtitle = t(:'talia.global.keywords')
   end
 end
