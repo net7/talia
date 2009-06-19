37e864f3cecc57fd0687b9cc6299ce8c35e4e46f
Luca Guidi <guidi.luca@gmail.com>
2009-06-19 11:33:02 +0200

    Rails 2.0.5 compat: Rails.root -> RAILS_ROOT

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -58,7 +58,7 @@ class ApplicationController < ActionController::Base
     def rescue_action_in_public(exception)
       case exception
       when ActiveRecord::RecordNotFound
-        render :file => "#{Rails.root}/public/404.html", :status => :not_found
+        render :file => "#{RAILS_ROOT}/public/404.html", :status => :not_found
       else
         super
       end
