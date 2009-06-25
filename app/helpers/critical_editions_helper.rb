7bb59674e496125928a1e528e15e2bc3c20a9c49
Daniel Hahn <ghub@limitedcreativity.org>
2009-06-25 17:30:05 +0200

    Small fix for print layout

--- app/helpers/critical_editions_helper.rb
+++ app/helpers/critical_editions_helper.rb
@@ -2,4 +2,15 @@ module CriticalEditionsHelper
   def print_title
     @book ? @book.title : edition_page_title
   end
+  
+  def custom_styles
+    styles = ''
+    if(@custom_edition_stylesheet)
+      @custom_edition_stylesheet.each do |style|
+        styles << stylesheet_link_tag(custom_style)
+      end
+    end
+    
+    styles
+  end
 end
