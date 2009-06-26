34233dcab93fd51a330b70e3a1b3ddc0e7468110
Luca Guidi <guidi.luca@gmail.com>
2009-06-26 15:25:47 +0200

    Sophiavision: removed tabs from the category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,7 +12,6 @@ class CategoriesController < ApplicationController
     raise(ArgumentError, "Unknow Category #{cat_uri}") unless(TaliaCore::Category.exists?(cat_uri))
     
     @category = TaliaCore::Category.find(cat_uri)
-    @all_categories = TaliaCore::Category.find(:all)
     @path = [ { :text => t(@category.name) } ]
     @title = t(:'talia.global.category') + " | #{t(@category.name)}"
     @subtitle = t(:'talia.global.category') 
