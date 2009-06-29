3692436271814ad11e047f188a1a2f217c53b1f7
tofani <robertox2k@gmail.com>
2009-06-29 10:44:17 +0200

    Now the print page have the same title of result page (see #1075)

--- app/helpers/critical_editions_helper.rb
+++ app/helpers/critical_editions_helper.rb
@@ -13,4 +13,19 @@ module CriticalEditionsHelper
     
     styles
   end
-end
+
+  def advanced_search_result_description
+    # add count and work
+    result = "<h2> #{@result_count} #{t(:'talia.search.result phrase 1')} <span class='red'> #{@words} </span>"
+    # add subpart
+    @searched_works.collect do |item|
+        result += " #{t(:'talia.search.result.optional.in the work')} '#{item[:work]}' #{t(:'talia.search.result.optional.in aphorisms')} #{item[:from]} #{t(:'talia.search.result.optional.through')} #{item[:to]},"
+      end unless @searched_works.nil?
+
+    # add edition title
+    result += "#{t(:'talia.search.result phrase 2')} #{@edition.hyper::title}</h2> "
+
+    # return result
+    return result
+    end
+  end
