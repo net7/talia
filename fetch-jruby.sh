1cb31424751df712147696c424dd5385ae5ca1e1
Daniel Hahn <ghub@limitedcreativity.org>
2009-07-29 10:14:49 +0200

    New jruby version and download script

--- fetch-jruby.sh
+++ fetch-jruby.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
+if test -z $1 ; then
+	DEFAULT_VERSION=1.3.1
+else
+	DEFAULT_VERSION=$1
+fi
+
+filename=jruby-$DEFAULT_VERSION.tgz
+
+if test -e vendor/jruby-$DEFAULT_VERSION ; then
+  echo Version $DEFAULT_VERSION already installed
+  exit 1
+fi
+
 cd vendor
-svn co http://svn.talia.discovery-project.eu/talia/trunk/jruby-1.1.5 jruby-1.1.5
+curl http://cloud.github.com/downloads/net7/talia/$filename > $filename
+tar xvzf $filename
+rm $filename
+
+if test -L jruby ; then
+  rm jruby
+fi
+
+ln -s jruby-$DEFAULT_VERSION jruby
+
+echo jruby $DEFAULT_VERSION installed
--- talia.sh
+++ talia.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
+
+# First parameter: shell or start, depending on if you want the shell or the server
+# Second parameter: Optional, may give a jruby version if multiple installed in vendor/
+
 pd=`dirname "$0"`
 prog_dir=`cd $pd && pwd`
-jruby_dir="vendor/jruby-1.1.5"
+
+if test -n "$2"; then
+	JRUBY_VERSION=$2
+fi
+
+if test -n "$JRUBY_VERSION"; then
+	jruby_special_version=-$JRUBY_VERSION
+else
+	jruby_special_version=''
+fi
+
+jruby_dir=vendor/jruby$jruby_special_version
+
 # We start the shell that the user is currently running
 shell=$SHELL
 
 if test -z $SHELL ; then
     # Fall back to bash
-    shell = '/bin/bash'
+    shellbq='/bin/bash'
 fi
 
 export PATH=$prog_dir/$jruby_dir/bin:$prog_dir/$jruby_dir/lib/ruby/gems/1.8/bin:$PATH
@@ -18,7 +34,7 @@ export PS1="talia-bash \w >> "
 case $1 in
 	shell)
 		echo ;
-		echo "Opening JRuby environment for Talia (using $shell)..."
+		echo "Opening JRuby environment for Talia (using $shell and jruby from $jruby_dir)..."
 		export TALIA_SHELL=true
 		$shell -i
 	;;
