422f56e58238805025ef13442bc0dcf0863ea5b1
tofani <robertox2k@gmail.com>
2009-11-28 17:14:19 +0100

    Update support for Europeana

--- lib/talia_util/europeana_importer/import_callback.rb
+++ lib/talia_util/europeana_importer/import_callback.rb
@@ -5,6 +5,13 @@ module TaliaUtil
       
       include TaliaUtil::Progressable
       
+      def before_import
+        run_with_progress("Deleting previous bibliographic cards", 1) do |progress|
+          # delete previous bibliographic cards
+          TaliaCore::BibliographicalCard.delete_all() { progress.inc }
+        end
+      end
+      
     end
   end
 end
