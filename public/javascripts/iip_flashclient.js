b520907832d684baf2b9992b1aa652ed089a5751
danilo giacomi <danilogiacomi@gmail.com>
2009-12-01 18:38:58 +0100

    new iip flash client

--- public/javascripts/iip_flashclient.js
+++ public/javascripts/iip_flashclient.js
@@ -10,15 +10,30 @@
  * with, height - dimension of the viewer window
  * element_id - the id of html element that will receive the viewer
  */
+//function load_iip_flashclient(server, image_path, width, height, element_id) {
+//    // TODO: Must be changed to work in subfolder URL
+//    var so = new SWFObject("/fliipish.swf", "flashZoomer", width, height, "9", "#e0e0e0");
+//    so.addParam("quality", "high");
+//    so.addParam("scale", "noscale");
+//    so.addParam("salign", "tl");
+//    so.addVariable("imgSrc", image_path);
+//    so.addVariable("zoomSteps", "3");
+//    so.addVariable("imgSrv", server);
+//    so.write(element_id);
+//}
+
 function load_iip_flashclient(server, image_path, width, height, element_id) {
     // TODO: Must be changed to work in subfolder URL
-    var so = new SWFObject("/fliipish.swf", "flashZoomer", width, height, "9", "#e0e0e0");
+    var so = new SWFObject("/iipclient.swf", "flashZoomer", width, height, '9', "#e0e0e0");
+    so.addParam("movie", "/iipclient.swf");
+    so.addParam("zoom_anim_duration", "2");
     so.addParam("quality", "high");
-    so.addParam("scale", "noscale");
+    so.addParam("bgcolor", "#f3f3f3");
+    so.addParam("allowScriptAccess", "sameDomain");
     so.addParam("salign", "tl");
-    so.addVariable("imgSrc", image_path);
-    so.addVariable("zoomSteps", "3");
-    so.addVariable("imgSrv", server);
-    so.write(element_id);		
-}
 
+    so.addVariable("image_path", image_path);
+    so.addVariable("iipserver", server);
+    so.addVariable("title", "");
+    so.write(element_id);
+}
